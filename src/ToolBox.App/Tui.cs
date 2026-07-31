f3f44635c52a87cea1cc82457e2857af4a95b7e4
luluzuri <luluzuri@student.42.fr>
2026-07-31 11:43:22 +0200

    [feature] Word Panel

--- src/ToolBox.App/Tui.cs
+++ src/ToolBox.App/Tui.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using Terminal.Gui.App;
 using Terminal.Gui.ViewBase;
 using Terminal.Gui.Views;
@@ -14,14 +15,32 @@ namespace ToolBox.App
 
 			app.Init();
 
-			Window window = new() { Title = "ToolBox (Esc to quit)" };
-			Label label = new()
+			ListView view = new()
 			{
-				Text = "Hello, Terminal.Gui v2",
-				X = Pos.Center(),
-				Y = Pos.Center()
+				X = 0,
+				Y = 0,
+				Width = Dim.Percent(30),
+				Height = Dim.Fill()
 			};
-			window.Add(label);
+			view.SetSource(new ObservableCollection<string>(registry.All().Select(x => x.Name)));
+
+			Label out_panel = new()
+			{
+				X = Pos.Right(view),
+				Y = 0,
+				Width = Dim.Fill(),
+				Height = Dim.Fill(),
+				Text = "Placeholder to replace"
+			};
+
+			StatusBar status_bar = new()
+			{
+
+			};
+
+			Window window = new() { Title = "ToolBox (Esc to quit)" };
+			window.Add(view);
+			window.Add(out_panel);
 			app.Run(window);
 		}
 	}
