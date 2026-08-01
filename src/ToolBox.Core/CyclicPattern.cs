20322acb9b0cc82de40cf0a00f3a435bfab8d605
luluzuri <luluzuri@student.42.fr>
2026-08-01 16:32:13 +0200

    [feature] finish Pattern

--- src/ToolBox.Core/CyclicPattern.cs
+++ src/ToolBox.Core/CyclicPattern.cs
@@ -4,28 +4,28 @@ namespace ToolBox.Core;
 
 public class CyclicPattern
 {
-	public static string Generate(int lenght)
+	public static string Generate(int length)
 	{
-		if (lenght <= 0) return "";
+		if (length <= 0) return "";
 
 		StringBuilder str = new();
 
 		for (char maj = 'A'; maj <= 'Z'; maj++)
 		{
-			for (char min = 'A'; min <= 'Z'; min++)
+			for (char min = 'a'; min <= 'z'; min++)
 			{
 				for (char n = '0'; n <= '9'; n++)
 				{
 					str.Append(maj);
 					str.Append(min);
 					str.Append(n);
-					if (str.Length == lenght) return str.ToString();
+					if (str.Length >= length) return str.ToString()[..length];
 				}
 
 			}
 		}
 
-		return str.ToString()[lenght..];
+		return str.ToString()[..length];
 	}
 
 	public static void FindOffset(int value)
--- tests/ToolBox.Tests/CyclicPatternTests.cs
+++ tests/ToolBox.Tests/CyclicPatternTests.cs
@@ -1,3 +1,33 @@
+using ToolBox.Core;
+
 namespace ToolBox.Tests;
 
+public class CyclicPatternTests
+{
+	[Fact]
+	public void Generate8Pattern()
+	{
+		string s = CyclicPattern.Generate(8);
 
+		Assert.Equal(8, s.Length);
+		Assert.Equal("Aa0Aa1Aa", s);
+	}
+
+	[Fact]
+	public void Generate3Pattern()
+	{
+		string s = CyclicPattern.Generate(3);
+
+		Assert.Equal(3, s.Length);
+		Assert.Equal("Aa0", s);
+	}
+
+	[Fact]
+	public void Generate0Pattern()
+	{
+		string s = CyclicPattern.Generate(0);
+
+		Assert.Equal(0, s.Length);
+		Assert.Equal("", s);
+	}
+}
