59f9fafee9a00f5bdbc83b45fffe1517b795ea50
luluzuri <luluzuri@student.42.fr>
2026-08-02 21:53:06 +0200

    [feature] Added basic header

--- src/ToolBox.App/UI/Layout/HeaderView.cs
+++ src/ToolBox.App/UI/Layout/HeaderView.cs
@@ -1,1 +1,17 @@
+using Terminal.Gui.ViewBase;
+
 namespace ToolBox.App.UI.Layout;
+
+public class HeaderView : View
+{
+	private readonly Label _shell;
+
+	public HeaderView()
+	{
+		_title = new Label { Text = "Header" };
+		Add(_title);
+	}
+
+	public void SetTitle(string text) => _title.Text = text;
+}
+
